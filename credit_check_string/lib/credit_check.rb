7e82b2dfa962c479945c8a1aed127108924ab309
JoelLindow <joellindow@gmail.com>
2017-05-06 21:59:28 -0600

    added split, reduce and double methods

--- credit_check_string/lib/credit_check.rb
+++ credit_check_string/lib/credit_check.rb
@@ -1,9 +1,43 @@
+require "pry"
 class CreditCheck
+attr_reader :card
 
   def initialize(card)
     @card = card
+
+  end
+
+  def split_card_number
+    card.split("")
+  end
+
+  def double_every_other_number
+    split_card_number.map.with_index do |num, ind|
+      if ind.even?
+        (num.to_i * 2).to_s
+      else
+        num
+      end
+    end
+  end
+
+  def split_card_with_reductions
+    double_every_other_number.map do |num|
+      if num.length >= 2
+        reduce_two_digit_number(num)
+      else
+        num
+      end
+    end
+  end
+
+  def reduce_two_digit_number(number)
+    split_up = number.split("")
+    split_string = split_up.map do |num|
+      num.to_i
+    end
+    split_string.inject(:+).to_s
   end
 
   
-
 end
--- credit_check_string/test/credit_check_test.rb
+++ credit_check_string/test/credit_check_test.rb
@@ -17,4 +17,21 @@ class CreditCheckTest < Minitest::Test
     assert_instance_of CreditCheck, cc
   end
 
+  def test_if_every_other_number_doubled
+    credit_card_number = "5541808923795240"
+    cc = CreditCheck.new(credit_card_number)
+    split_card_with_doubles = ["10", "5", "8", "1", "16", "0", "16", "9",
+                               "4", "3", "14", "9", "10", "2", "8", "0"]
+    assert_equal split_card_with_doubles, cc.double_every_other_number
+  end
+
+  def test_it_can_reduce_two_digit_numbers_properly
+    credit_card_number = "5541808923795240"
+    cc = CreditCheck.new(credit_card_number)
+    split_card_with_reductions = ["1", "5", "8", "1", "7", "0", "7", "9",
+                                  "4", "3", "5", "9", "1", "2", "8", "0"]
+    assert_equal split_card_with_reductions, cc.split_card_with_reductions
+  end
+
+  
 end
